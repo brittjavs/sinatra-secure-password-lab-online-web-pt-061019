3b98999537f4932a3b54eba8f97dcbe5d8d8cdd6
Brittany Javalera <brittjavalera@gmail.com>
2019-09-25 18:38:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    user = User.create(:username => params[:username], :password => params[:password])
-		if user
+    user = User.new(:username => params[:username], :password => params[:password])
+		if user.save
 			redirect "/login"
 		else
 			redirect "/failure"
